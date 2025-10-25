0effa5ee9bd74c42c64fe2f45807fa85b59ac2f0
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-25 20:04:33 +0600

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stream_provider/stream_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,ref) {
+    final provider = ref.watch(stockPriceProvider);
     return const Placeholder();
   }
 }
