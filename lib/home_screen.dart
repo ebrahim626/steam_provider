e55a75a519cf1d4fa03bfcf90c155d12d3e3ad9e
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-25 20:13:06 +0600

    updatd

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,11 +6,22 @@ class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context,ref) {
+  Widget build(BuildContext context, ref) {
     final provider = ref.watch(stockPriceProvider);
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text('StreamProvider')),),
-      body: Center(child: Text('$provider'),),
+      appBar: AppBar(title: Center(child: Text('StreamProvider'))),
+      body: Center(
+        child: provider.when(
+          data: (price) {
+            return Text(
+              '$price',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            );
+          },
+          error: (e, stack) => Text('Error'),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
